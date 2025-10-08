a310b9aab12c2bd2162d6de0deb27bf0c0977701
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-10-09 00:50:55 +0300

    did the same for categories

---
+++ apps/product-service/src/controllers/category.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export const createCategory = async (req: Request, res: Response) => {};
+
+export const updateCategory = async (req: Request, res: Response) => {};
+
+export const deleteCategory = async (req: Request, res: Response) => {};
+
+export const getCategories = async (req: Request, res: Response) => {};
+
+export const getCategory = async (req: Request, res: Response) => {};
--- apps/product-service/src/routes/category.route.ts
+++ apps/product-service/src/routes/category.route.ts
@@ -1,8 +1,21 @@
-import { Request, Response, Router } from "express";
+// routes/category.route.ts
+
+import { Router } from "express";
+import {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategories,
+  getCategory,
+} from "../controllers/category.controller"; // Import controller functions
 
 const router: Router = Router();
 
-router.get("/test", (req: Request, res: Response) => {
-  res.json({ message: "works fine" });
-});
+// Define routes and link them to the controller functions
+router.get("/", getCategories); // GET all categories
+router.get("/:id", getCategory); // GET a single category by ID
+router.post("/", createCategory); // POST to create a new category
+router.put("/:id", updateCategory); // PUT to update an existing category
+router.delete("/:id", deleteCategory); // DELETE a category by ID
+
 export default router;

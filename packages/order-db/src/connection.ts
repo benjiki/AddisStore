5a763f60cac6dcb5b5c575fa32d8f8e75fbc8b42
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-10-09 10:26:17 +0300

    order-db

---
+++ packages/order-db/src/connection.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectOrderDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error("Mongo db url dose not exist!!");
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};

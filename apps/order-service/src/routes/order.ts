25d7f7292bf2e7a2638a9ce82c7b883d6213c707
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-10-09 11:01:10 +0300

    order router

---
+++ apps/order-service/src/routes/order.ts
@@ -0,0 +1,18 @@
+import { FastifyInstance } from "fastify";
+import { shouldBeUser } from "../../middleware/authMiddleware";
+import { Order } from "@repo/order-db";
+import { request } from "http";
+export const orderRoute = async (fastify: FastifyInstance) => {
+  fastify.get(
+    "/user-order",
+    { preHandler: shouldBeUser },
+    async (request, reply) => {
+      const orders = await Order.find({ userId: request.userId });
+      return reply.send(orders);
+    }
+  );
+  fastify.get("/orders", async (request, reply) => {
+    const orders = await Order.find();
+    return reply.send(orders);
+  });
+};
